aae707e0cfec1a74f9c638fc14a3b7ce90dbf9ae
liakatbiswas <87108284+liakatbiswas@users.noreply.github.com>
2023-01-29 23:23:58 +0600

    class 2 commenting done

--- simple-scroll-to-top-wp.php
+++ simple-scroll-to-top-wp.php
@@ -1,3 +1,16 @@
 <?php
-
+/**
+ * Plugin Name:       Simple Scroll To Top WP
+ * Plugin URI:        https://wordpress.org/plugins/simple-scroll-to-top-wp/
+ * Description:       Lorem ipsum dolor sit amet consectetur adipiscing elit himenaeos nisi mus quisque facilisis aliquam, netus posuere fames cursus libero taciti vestibulum nostra auctor hendrerit viverra.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Md. Liakat Biswas
+ * Author URI:        http://liakatbiswas.com
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/liakatbiswas
+ * Text Domain:       sstt
+ */
 ?>
